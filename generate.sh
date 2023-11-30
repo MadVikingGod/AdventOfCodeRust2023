167ccdc7c95f9de214f15a85a058996f8ce7fffe
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-11-30 14:13:48 -0600

    Basic setup

---
+++ generate.sh
@@ -0,0 +1,23 @@
+if [ -z $1 ]; then
+    echo Please supply a day number
+    exit 1
+fi
+day="$(printf '%02d' $1)"
+
+bin="src/bin/day_${day}.rs"
+input="src/bin/input/day_${day}.txt"
+touch ${bin} ${input}
+if [ ! -s ${bin} ]; then
+    cat >$bin <<EOF
+fn main() {
+    let input = include_str!("input/day_01.txt");
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+#[test]
+fn test_nothing() {
+    ()
+}
+EOF
+fi
---
+++ src/bin/day_01.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let _input = include_str!("input/day_01.txt");
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+#[test]
+fn test_nothing() {
+    ()
+}
