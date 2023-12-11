4fdb0d0f7c733f633b15a4a561f0298e03d1de65
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-12-11 08:19:13 -0600

    refactor day 11

--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -17,45 +17,21 @@ fn main() {
 }
 
 fn part1(input: Img) -> i32 {
-    let max_x = input.keys().map(|p| p.x).max().unwrap();
-    let max_y = input.keys().map(|p| p.y).max().unwrap();
-    let xs: HashSet<i32> = input.keys().map(|p| p.x).collect();
-    let ys: HashSet<i32> = input.keys().map(|p| p.y).collect();
-    let no_x: Vec<i32> = (0..=max_x).filter(|x| !xs.contains(x)).collect();
-    let no_y: Vec<i32> = (0..=max_y).filter(|y| !ys.contains(y)).collect();
-    let xlate = Translation { x: no_x, y: no_y };
-
-    input
-        .keys()
-        .map(|p| xlate.expand(p))
-        .collect::<Vec<_>>()
-        .iter()
-        .tuple_combinations()
-        .map(|(p1, p2)| p1 - p2)
-        .map(|p| p.magnitude())
-        .sum()
+    part2(input, 2) as i32
 }
 
 fn part2(input: Img, scale: i64) -> i64 {
-    let max_x = input.keys().map(|p| p.x).max().unwrap();
-    let max_y = input.keys().map(|p| p.y).max().unwrap();
-    let xs: HashSet<i32> = input.keys().map(|p| p.x).collect();
-    let ys: HashSet<i32> = input.keys().map(|p| p.y).collect();
-    let no_x: Vec<i32> = (0..=max_x).filter(|x| !xs.contains(x)).collect();
-    let no_y: Vec<i32> = (0..=max_y).filter(|y| !ys.contains(y)).collect();
-    let xlate = Translation { x: no_x, y: no_y };
+    let xlate = Translation::new(&input);
 
     input
         .keys()
-        .map(|p| xlate.expand_scale(p, scale))
+        .map(|p| xlate.expand(p, scale))
         .collect::<Vec<_>>()
         .iter()
         .tuple_combinations()
         .map(|(p1, p2)| p1 - p2)
         .map(|p| p.magnitude())
         .sum()
-
-    //702152906986 is too high
 }
 
 struct Translation {
@@ -64,12 +40,17 @@ struct Translation {
 }
 
 impl Translation {
-    fn expand(&self, p: &Point<i32>) -> Point<i32> {
-        let x = p.x + self.x.iter().filter(|x| p.x > **x).count() as i32;
-        let y = p.y + self.y.iter().filter(|y| p.y > **y).count() as i32;
-        Point { x, y }
+    fn new(input: &Img) -> Translation {
+        let max_x = input.keys().map(|p| p.x).max().unwrap();
+        let max_y = input.keys().map(|p| p.y).max().unwrap();
+        let xs: HashSet<i32> = input.keys().map(|p| p.x).collect();
+        let ys: HashSet<i32> = input.keys().map(|p| p.y).collect();
+        let no_x: Vec<i32> = (0..=max_x).filter(|x| !xs.contains(x)).collect();
+        let no_y: Vec<i32> = (0..=max_y).filter(|y| !ys.contains(y)).collect();
+        Translation { x: no_x, y: no_y }
     }
-    fn expand_scale(&self, p: &Point<i32>, scale: i64) -> Point<i64> {
+
+    fn expand(&self, p: &Point<i32>, scale: i64) -> Point<i64> {
         let x = p.x as i64 + self.x.iter().filter(|x| p.x > **x).count() as i64 * (scale - 1);
         let y = p.y as i64 + self.y.iter().filter(|y| p.y > **y).count() as i64 * (scale - 1);
         Point { x, y }
